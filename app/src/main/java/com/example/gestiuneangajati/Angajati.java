2f9e21be060706d61fb083473546d8d8c1720ac2
Alexandra <alexandra_florentina22@yahoo.com>
2019-05-08 13:39:43 +0300

    Profil manager
    Design + intenturi

---
+++ app/src/main/java/com/example/gestiuneangajati/Angajati.java
@@ -0,0 +1,13 @@
+package com.example.gestiuneangajati;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Angajati extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_angajati);
+    }
+}
--- app/src/main/java/com/example/gestiuneangajati/ProfilManager.java
+++ app/src/main/java/com/example/gestiuneangajati/ProfilManager.java
@@ -4,19 +4,60 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class ProfilManager extends AppCompatActivity {
+    private ImageButton actualizarePoza;
+    private CardView angajati;
+    private CardView agenda;
+    private CardView progres;
+    private CardView setari;
+    private Button deconectare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil_manager);
+
+        actualizarePoza = findViewById(R.id.actualizareImagineManager);
+        angajati = findViewById(R.id.cvAngajatiiMei);
+        setari = findViewById(R.id.cvSetariManager);
+        agenda = findViewById(R.id.cvAgenda);
+        progres = findViewById(R.id.cvProgres);
+        deconectare = findViewById(R.id.deconectareManager);
+    }
+
+    public void angajatiClick(View view){
+        Intent i = new Intent(ProfilManager.this, Angajati.class);
+        startActivity(i);
+    }
+
+    public void setariMClick(View view){
+        Intent i = new Intent(ProfilManager.this, SetariProfilManager.class);
+        startActivity(i);
+    }
+
+    public void agendaClick(View view){
+        Intent i = new Intent(ProfilManager.this, Agenda.class);
+        startActivity(i);
+    }
+
+    public void progresClick(View view){
+        Intent i = new Intent(ProfilManager.this, Progres.class);
+        startActivity(i);
+    }
+
+    public void deconectareMClick(View view){
+        Intent i = new Intent(ProfilManager.this, MainActivity.class);
+        startActivity(i);
+        this.finish();
     }
 
 }
