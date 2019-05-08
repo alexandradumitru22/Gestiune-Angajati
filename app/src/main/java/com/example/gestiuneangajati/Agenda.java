619c4d259ee76a59607f86b279bac602bae4fef4
Alexandra <alexandra_florentina22@yahoo.com>
2019-05-08 21:44:13 +0300

    Lista activitati angajat + agenda

--- app/src/main/java/com/example/gestiuneangajati/Agenda.java
+++ app/src/main/java/com/example/gestiuneangajati/Agenda.java
@@ -2,12 +2,72 @@ package com.example.gestiuneangajati;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Agenda extends AppCompatActivity {
+    private EditText agenda;
+    private Button salveza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
+
+        agenda = findViewById(R.id.agenda);
+
+        agenda.setText(Deschide("agenda.txt"));
     }
+
+    public void Salveaza(String fileName) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(agenda.getText().toString());
+            out.close();
+            Toast.makeText(this, "Nota salvata!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exceptie: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String Deschide(String fileName) {
+        String content = "";
+        if (FileExists(fileName)) {
+            try {
+                InputStream in = openFileInput(fileName);
+                if ( in != null) {
+                    InputStreamReader tmp = new InputStreamReader( in );
+                    BufferedReader reader = new BufferedReader(tmp);
+                    String str;
+                    StringBuilder buf = new StringBuilder();
+                    while ((str = reader.readLine()) != null) {
+                        buf.append(str + "\n");
+                    } in .close();
+                    content = buf.toString();
+                }
+            } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+                Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+        return content;
+    }
+
+    public boolean FileExists(String fname) {
+        File file = getBaseContext().getFileStreamPath(fname);
+        return file.exists();
+    }
+
+    public void salveazaClick(View view){
+        Salveaza("agenda.txt");
+    }
+
 }
