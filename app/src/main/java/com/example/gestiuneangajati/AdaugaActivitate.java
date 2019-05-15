63397e9e5babc4e23cd12ebf3be4b03cf606119d
Alexandra <alexandra_florentina22@yahoo.com>
2019-05-15 15:42:39 +0300

    Dependinte firebase
    samba pola an steaua

---
+++ app/src/main/java/com/example/gestiuneangajati/AdaugaActivitate.java
@@ -0,0 +1,13 @@
+package com.example.gestiuneangajati;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdaugaActivitate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adauga_activitate);
+    }
+}
